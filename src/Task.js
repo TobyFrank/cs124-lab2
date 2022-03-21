bc9ba24952f2bea781d9df97f0bfa0ebbff200ba
tobyfrank <tfrank@hmc.edu>
2022-03-21 13:20:13 -0700

    Began adding priority icons

--- src/Task.js
+++ src/Task.js
@@ -1,21 +1,35 @@
 import "./Task.css";
 import editIcon from "./edit.png";
 import deleteIcon from "./delete.png";
+// import lowPriorityIcon "./lowPriority.png";
+// import medPriorityIcon "./medPriority.png";
+// import highPriorityIcon "./highPriority.png";
+
+import {useState} from "react";
 
 function Task(props) {
+    const priorityDict = {
+        1: lowPriorityIcon,
+        2: medPriorityIcon,
+        3: highPriorityIcon
+    }
+    const taskData = props.taskData;
+    const [editingTaskText, setEditingTaskText] = useState(taskData.text);
     return (
         <div className="listItem" id={props.isChecked ? "completedTask" : "incompleteTask"}>
-            <input type="checkbox" checked={props.isChecked} onChange={(e) => props.onCompletedTask(props.id)}></input>
-            {props.editingTaskId === props.id ?
-                <input className={props.id === props.editingTaskId ? "task editing" : "task"}
+            <input type="checkbox" checked={props.isChecked} onChange={(e) => props.onCompletedTask(taskData.id)}></input>
+            {props.editingTaskId === taskData.id ?
+                <input className={taskData.id === props.editingTaskId ? "task editing" : "task"}
                        onChange={
-                           (e) => props.onEditTask(props.id, "name", e.target.value)
+                           (e) => setEditingTaskText(e.target.value)
                        }
-                       value={props.text["name"]}></input> :
-                <span className="taskName">{props.text}</span>
+                       onKeyDown={(e) => (e.code === "Enter") && props.onEditTask(taskData.id, "text", editingTaskText)}
+                       value={editingTaskText}></input> :
+                <span className="taskText">{editingTaskText}</span>
             }
-            <img className="editIcon" src={editIcon} alt="edit" onClick={(e ) => props.onToggleEditTaskId(props.id)}></img>
-            <img className="deleteIcon" src={deleteIcon} alt="delete" onClick={(e) => props.toggleModal(props.id)}></img>
+            <img className="priorityIcon" src={priorityDict[taskData.priority]} alt={priorityDict[taskData.priority]} onClick={(e ) => console.log("priority change")}></img>
+            <img className="editIcon" src={editIcon} alt="edit" onClick={(e ) => props.onEditTask(taskData.id, "text", editingTaskText)}></img>
+            <img className="deleteIcon" src={deleteIcon} alt="delete" onClick={(e) => props.toggleModal(taskData.id)}></img>
         </div>
     )
 }
