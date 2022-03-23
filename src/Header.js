56188334e69163b46203cc5f0ed25216ade9c3ca
tobyfrank <tfrank@hmc.edu>
2022-03-22 20:49:47 -0700

    fixing errors for pull request

--- src/Header.js
+++ src/Header.js
@@ -17,30 +17,30 @@ function Header(props) {
                     <img className={"filterIcon"} src={filterIcon}></img>
                     </button>
                 {props.showSortDropdown && <div className={"paramDropdown"}>
-                    <a className={"sortDirection"}
+                    <span className={"sortDirection"}
                        onClick={(e) => {
                            props.onSortDirectionToggle();
                            e.stopPropagation();
                            e.preventDefault();
-                       }}>{props.sortDirection === "asc" ? "^ Sort Asc ^" : "v Sort Desc v"}</a>
-                    <a className={props.sortParam === "text" ? "currentParam" : "param"}
+                       }}>{props.sortDirection === "asc" ? "^ Sort Asc ^" : "v Sort Desc v"}</span>
+                    <span className={props.sortParam === "text" ? "currentParam" : "param"}
                        onClick={(e) => {
                            props.onSortParamChange("text");
                            e.stopPropagation();
                            e.preventDefault();
-                       }}>Alphabetical</a>
-                    <a className={props.sortParam === "priority" ? "currentParam" : "param"}
+                       }}>Alphabetical</span>
+                    <span className={props.sortParam === "priority" ? "currentParam" : "param"}
                        onClick={(e) => {
                            props.onSortParamChange("priority");
                            e.stopPropagation();
                            e.preventDefault();
-                       }}>Priority</a>
-                    <a className={props.sortParam === "created" ? "currentParam" : "param"}
+                       }}>Priority</span>
+                    <span className={props.sortParam === "created" ? "currentParam" : "param"}
                        onClick={(e) => {
                            props.onSortParamChange("created");
                            e.stopPropagation();
                            e.preventDefault();
-                       }}>Newest</a>
+                       }}>Add Date</span>
                 </div>}
             </div>
         </div>
